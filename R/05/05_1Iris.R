c8b558045f9ec643fa8e09e388311654ecbda2ab
4562sky <4562sky@naver.com>
2022-12-19 18:02:26 +0900

    타이타닉

---
+++ R/05/05_1Iris.R
@@ -0,0 +1,113 @@
+#R 내장 데이터 가져오기
+data(iris)
+
+#iris 데이터 확인
+str(iris)
+
+#iris : 꽃받침, 꽃잎 데이터 추출
+iris
+names(iris1)
+iris1 <- iris[, -5]
+
+#기술통계량
+summary(iris1) 
+
+#상관계수
+cor(iris1, method="pearson")
+
+#색의 농도로 상관계수 
+install.packages("corrgram")
+library(corrgram)
+
+corrgram(iris1, upper.panel = panel.conf)
+
+#상관계수 챠트
+install.packages("PerformanceAnalytics")
+library(PerformanceAnalytics)
+
+chart.Correlation(iris1)
+
+#학습데이터와 테스트데이터 분리
+1:nrow(iris1)
+x <- sample(1:nrow(iris1), 0.7 * nrow(iris1))
+
+train <- iris1[x, ]
+test <- iris1[-x, ]
+
+nrow(iris1)
+nrow(test)
+
+####회귀모델 : 꽃받침 길이 예측 
+names(iris1)
+
+#학습
+model1 <- lm(formula = Sepal.Length ~  Sepal.Width + Petal.Length + Petal.Width, data=train)
+model2 <- lm(formula = Sepal.Length ~  Petal.Length + Petal.Width, data=train)
+model3 <- lm(formula = Sepal.Length ~  Sepal.Width + Petal.Length, data=train)
+model4 <- lm(formula = Sepal.Length ~  Sepal.Width + Petal.Width, data=train)
+
+#예측
+pred1 <- predict(model1, test)
+pred2 <- predict(model2, test)
+pred3 <- predict(model3, test)
+pred4 <- predict(model4, test)
+
+#평가
+#RMSE : sqrt((실제 - 예측)^2의 평균)
+
+RMSE1 <- sqrt(mean((test$Sepal.Length - pred1)^2))
+RMSE2 <- sqrt(mean((test$Sepal.Length - pred2)^2))
+RMSE3 <- sqrt(mean((test$Sepal.Length - pred3)^2))
+RMSE4 <- sqrt(mean((test$Sepal.Length - pred4)^2))
+
+cat("예측1: ",RMSE1)
+cat("예측2: ",RMSE2)
+cat("예측3: ",RMSE3)
+cat("예측4: ",RMSE4)
+
+#분류모델 
+library(ggplot2)
+ggplot(iris, aes(Sepal.Length, Sepal.Width))  + 
+  geom_point(aes(colour = Species))
+
+ggplot(iris, aes(Petal.Length , Petal.Width))  + 
+  geom_point(aes(colour = Species))
+
+#학습데이터와 테스트데이터 나누기
+data(iris)
+train <- iris[x, ]
+test <- iris[-x, ]
+
+#########모델 학습
+#트리 모델 
+install.packages("party")
+library(party)
+names(iris)
+
+model1 <- ctree(Species ~ Sepal.Length + Petal.Length + Petal.Width, data = train)
+model2 <- ctree(Species ~ Sepal.Length + Petal.Length, data = train)
+model3 <- ctree(Species ~ Sepal.Length + Petal.Width, data = train)
+
+plot(model)
+
+#예측 
+pred1 <- predict(model1, test)
+pred2 <- predict(model2, test)
+pred3 <- predict(model3, test)
+
+#혼돈행렬
+t1 <- table(test$Species, pred1) 
+t2 <- table(test$Species, pred2) 
+t3 <- table(test$Species, pred3) 
+
+# 정확도
+acc1 <- (t1[1,1] + t1[2,2] + t1[3,3]) / sum(t1)
+acc2 <- (t2[1,1] + t2[2,2] + t2[3,3]) / sum(t2)
+acc3 <- (t3[1,1] + t3[2,2] + t3[3,3]) / sum(t3)
+
+acc1
+acc2
+acc3
+
+#iris data 저장
+ 
---
+++ R/05/05_2Titanic.R
@@ -0,0 +1,108 @@
+#타이타닉 데이터
+#https://www.kaggle.com/c/titanic/overview
+
+#데이터불러오기
+getwd()
+setwd("C:\Rwork\05")
+
+df <- read.csv("./05_titanic.csv", 
+                  header = T, 
+                  stringsAsFactors = F, 
+                  fileEncoding = 'euc-kr') 
+head(df)
+#데이터구조
+str(df)
+
+names(df)
+# PassengerID	승객을 구별하는 고유 ID number	Int
+# Survived	승객의 생존 여부를 나타내며 생존은 1, 사망은 0 입니다.	Factor
+df$Survived <- as.factor(ifelse(df$Survived == 0, df$Survived <- "NO", "YES"))
+
+# Pclass	선실의 등급으로서 1등급(1)부터 3등급(3)까지 3개 범주입니다.	Ord.Factor
+df$Pclass <- as.factor(df$Pclass)
+
+# Name	승객의 이름	Factor
+df$Name <- as.factor(df$Name)
+
+# Sex	승객의 성별	Factor
+df$Sex <- as.factor(df$Sex)
+
+# Age	승객의 나이	Numeric
+df$Age <- as.numeric(df$Age)
+
+# SibSp	각 승객과 동반하는 형제 또는 배우자의 수를 설명하는 변수이며 0부터 8까지 존재합니다.	Integer
+df$SibSp <- as.integer(df$SibSp)
+
+# Parch	각 승객과 동반하는 부모님 또는 자녀의 수를 설명하는 변수이며 0부터 9까지 존재합니다.	Integer
+df$Parch <- as.integer(df$Parch)
+
+# Ticket	승객이 탑승한 티켓에 대한 문자열 변수	Factor
+df$Ticket <- as.factor(df$Ticket)
+
+# Fare	승객이 지금까지 여행하면서 지불한 금액에 대한 변수	Numeric
+df$Fare <- as.numeric(df$Fare)
+
+# Cabin	각 승객의 선실을 구분하는 변수이며 범주와 결측치가 너무 많습니다.	Factor
+df$Cabin <- as.factor(df$Cabin)
+
+# Embarked	승선항, 출항지를 나타내며 C, Q, S 3개 범주이다.	Factor
+df$Embarked <- as.factor(df$Embarked)
+
+# 결측치 확인
+str(df)
+
+sapply(df, FUN = function(x) {
+  sum(is.na(x))
+}) 
+ 
+# 결측치 처리
+# 평균값으로 대체
+df$Age <- ifelse(is.na(df$Age) == TRUE, mean(df$Age, na.rm = TRUE), df$Age) 
+
+# 성별에 따른 생존여부
+library(ggplot2)
+class(df$Survived)
+mode(df$Survived)
+
+ggplot_sex <- ggplot(df, aes(x = Survived,fill = Sex, width = .8)) + 
+  geom_bar()
+ggplot_sex
+
+#Pclass	선실의 등급에 따른 생존여부
+ggplot_Pclass <- ggplot(df, aes(x = Survived,fill = Pclass)) + 
+  geom_bar()
+
+ggplot_Pclass
+
+#분류모델
+names(df)
+ta <- df[,c("Pclass", "Sex", "Age", "Survived")]
+1:nrow(ta)
+x <- sample(1:nrow(ta), 0.7 * nrow(ta))
+
+train <- ta[x, ]
+test <- ta[-x, ]
+
+#학습
+library(party)
+model <- ctree(Survived ~ Pclass + Sex + Age, data = train)
+
+#예측
+pred <- predict(model, test)
+ 
+#혼돈행렬
+t <- table(test$Survived, pred)
+t
+
+#Accuracy
+acc <- (t[1,1] + t[2,2]) /sum(t)
+acc
+
+
+ 
+
+#혼돈행렬
+ 
+ 
+#accuracy
+ 
